9f6cd1ae30b3d9cc80bb953a6ce8fda3c295d8e9
BhraJoe <josephatanga@gmail.com>
2024-07-09 01:42:48 +0000

    t

--- script.js
+++ script.js
@@ -1,37 +1,53 @@
-let just  = document.getElementById("just")
-let popular  = document.getElementById("popular")
-let tv  = document.getElementById("tv")
-let free  = document.getElementById("free")
+let just  = document.getElementById("just_tab")
+let popular  = document.getElementById("popular_tab")
+let tv  = document.getElementById("tv_tab")
+let free  = document.getElementById("free_tab")
 
-document.getElementById("just_tab").style.display = "block";
-document.getElementById("popular_tab").style.display = "none";
-document.getElementById("tv_tab").style.display = "none";
-document.getElementById("free_tab").style.display = "none";
+document.getElementById("just").style.display = "block";
+document.getElementById("popular").style.display = "none";
+document.getElementById("tv").style.display = "none";
+document.getElementById("free").style.display = "none";
 
 just.onclick = ()=> {
-    document.getElementById("just_tab").style.display = "block";
-    document.getElementById("popular_tab").style.display = "none";
-    document.getElementById("tv_tab").style.display = "none";
-    document.getElementById("free_tab").style.display = "none"; 
+    document.getElementById("just").style.display = "block";
+    document.getElementById("popular").style.display = "none";
+    document.getElementById("tv").style.display = "none";
+    document.getElementById("free").style.display = "none"; 
+    just.style.borderBottom = "1px solid red"
+    popular.style.borderBottom = "white"
+    tv.style.borderBottom = "white"
+    free.style.borderBottom = "white"
 }
 
 popular.onclick = ()=> {
-    document.getElementById("just_tab").style.display = "none";
-    document.getElementById("popular_tab").style.display = "block";
-    document.getElementById("tv_tab").style.display = "none";
-    document.getElementById("free_tab").style.display = "none"; 
+    document.getElementById("just").style.display = "none";
+    document.getElementById("popular").style.display = "block";
+    document.getElementById("tv").style.display = "none";
+    document.getElementById("free").style.display = "none"; 
+    just.style.borderBottom = "white"
+    popular.style.borderBottom = "1px solid red"
+    tv.style.borderBottom = "white"
+    free.style.borderBottom = "white"
 }
 
 tv.onclick = ()=> {
-    document.getElementById("just_tab").style.display = "none";
-    document.getElementById("popular_tab").style.display = "none";
-    document.getElementById("tv_tab").style.display = "block";
-    document.getElementById("free_tab").style.display = "none"; 
+    document.getElementById("just").style.display = "none";
+    document.getElementById("popular").style.display = "none";
+    document.getElementById("tv").style.display = "block";
+    document.getElementById("free").style.display = "none"; 
+    just.style.borderBottom = "white"
+    popular.style.borderBottom = "white"
+    tv.style.borderBottom = "1px solid red"
+    free.style.borderBottom = "white"
 }
 
 free.onclick = ()=> {
-    document.getElementById("just_tab").style.display = "none";
-    document.getElementById("popular_tab").style.display = "none";
-    document.getElementById("tv_tab").style.display = "none";
-    document.getElementById("free_tab").style.display = "block"; 
+    document.getElementById("just").style.display = "none";
+    document.getElementById("popular").style.display = "none";
+    document.getElementById("tv").style.display = "none";
+    document.getElementById("free").style.display = "block"; 
+    just.style.borderBottom = "white"
+    popular.style.borderBottom = "white"
+    tv.style.borderBottom = "white"
+    free.style.borderBottom = "1px solid red"
 }
